f32d2ee6e80e664638c077e3f25c68a14c2df902
Luca Bäck <luca.baeck@sap.com>
2026-01-27 21:06:01 +0100

    Refactor: Update user accounts and VM details outputs for shared VM configuration

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,10 +6,10 @@
 output "user_accounts" {
   description = "[CONTRACT] User accounts mit Login-Informationen"
   sensitive   = true # Enthält Passwörter
-  value = local.vm_count > 0 ? {
-    for i in range(local.vm_count) : local.user_ids[i] => {
+  value = length(local.all_users) > 0 ? {
+    for i in range(length(local.all_users)) : local.user_ids[i] => {
       type     = "password"
-      ip       = local.enable_floating_ip ? openstack_networking_floatingip_v2.fip[i].address : openstack_compute_instance_v2.student_vms[i].network[0].fixed_ip_v4
+      ip       = local.enable_floating_ip ? openstack_networking_floatingip_v2.fip[0].address : openstack_compute_instance_v2.shared_vm.network[0].fixed_ip_v4
       port     = 22
       username = local.usernames[i]
       auth     = random_password.user_passwords[i].result
@@ -24,15 +24,18 @@ output "user_accounts" {
 ############################
 
 output "vm_details" {
-  description = "Details aller User-VMs"
+  description = "Details der gemeinsamen VM und aller Benutzer"
   value = local.vm_count > 0 ? {
-    for i in range(local.vm_count) : local.user_ids[i] => {
-      instance_id   = openstack_compute_instance_v2.student_vms[i].id
-      instance_name = openstack_compute_instance_v2.student_vms[i].name
-      fixed_ip      = openstack_compute_instance_v2.student_vms[i].network[0].fixed_ip_v4
-      floating_ip   = local.enable_floating_ip ? openstack_networking_floatingip_v2.fip[i].address : null
-      ssh_command   = local.enable_floating_ip ? "ssh ${local.usernames[i]}@${openstack_networking_floatingip_v2.fip[i].address}" : "ssh ${local.usernames[i]}@${openstack_compute_instance_v2.student_vms[i].network[0].fixed_ip_v4}"
-      team          = local.all_users[i].team
+    shared_vm = {
+      instance_id   = openstack_compute_instance_v2.shared_vm.id
+      instance_name = openstack_compute_instance_v2.shared_vm.name
+      fixed_ip      = openstack_compute_instance_v2.shared_vm.network[0].fixed_ip_v4
+      floating_ip   = local.enable_floating_ip ? openstack_networking_floatingip_v2.fip[0].address : null
+      users         = [for i in range(length(local.all_users)) : {
+        username    = local.usernames[i]
+        team        = local.all_users[i].team
+        ssh_command = local.enable_floating_ip ? "ssh ${local.usernames[i]}@${openstack_networking_floatingip_v2.fip[0].address}" : "ssh ${local.usernames[i]}@${openstack_compute_instance_v2.shared_vm.network[0].fixed_ip_v4}"
+      }]
     }
   } : {}
 }
@@ -41,9 +44,10 @@ output "users_summary" {
   description = "Übersicht: Anzahl VMs und User"
   value = {
     vm_count  = local.vm_count
+    user_count = length(local.all_users)
     usernames = local.usernames
     emails    = local.emails
-    teams     = distinct([for user in local.all_users : user.team])
+    teams     = local.unique_teams
   }
 }
 
